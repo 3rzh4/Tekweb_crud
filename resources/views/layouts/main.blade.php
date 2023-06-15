9d2c2420bd2052a88011359987104a94b96f2557
Leinxa <lefinakawaii@gmail.com>
2023-06-15 11:17:34 +0700

    bug fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,8 +41,8 @@
           <hr class="sidebar-divider my-0">
     
           <!-- Nav Item - Dashboard -->
-          <li class="nav-item {{ (request()->is('admin')) ? 'active' : '' }}">
-            <a class="nav-link" href="/admin">
+          <li class="nav-item {{ (request()->is('dashboard')) ? 'active' : '' }}">
+            <a class="nav-link" href="/dashboard">
               <i class="fas fa-fw fa-tachometer-alt"></i>
               <span>Dashboard</span></a>
           </li>
